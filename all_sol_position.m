0c0596e85dd94151306da23607eb1821ef7b8d4b
Flook <flook11223344@gmail.com>
2026-01-26 06:00:26 +0700

    Update all_sol_position.m

--- all_sol_position.m
+++ all_sol_position.m
@@ -3,256 +3,216 @@ close all
 clc
 
 % ==========================================
-% LOOP 1: Green-Grey-Yellow
-% Input: Theta 3 (Yellow) = 19.94
-% Output: Theta 2 (Green), Theta 4 (Grey)
+% SECTION 1: PARAMETERS & INPUT
 % ==========================================
+% --- Dimensions (Meters) ---
+L1 = 0.210; % Ground (d) - Pink
+L2_Loop1 = 0.180; % Crank Loop 1 (a) - Green
+L3_Loop1 = 0.180; % Coupler Loop 1 (b) - Yellow
+L4_Shared = 0.118; % Shared Rocker (c) - Grey
 
-% --- 1. Parameters ---
-L1 = 0.210; % d
-L2 = 0.180; % a (Green)
-L3 = 0.180; % b (Yellow)
-L4 = 0.118; % c (Grey)
-a = L2; b = L3; c = L4; d = L1;
+L2_Loop2 = 0.118; % Crank Loop 2 (Cyan)
+L3_Loop2 = 0.210; % Coupler Loop 2 (Red)
 
-offset = deg2rad(0.81);
-q3d_global = 19.94;
+L2_Loop3 = 0.118; % Crank Loop 3 (Cyan - Extended)
+L3_Loop3 = 0.210; % Coupler Loop 3 (Blue)
+L4_Loop3 = 0.118; % Rocker Loop 3 (Brown)
+
+d = L1; % Common Ground
+
+% --- Global Input ---
+offset_deg = 0.81;
+offset = deg2rad(offset_deg);
+
+q3d_global = 19.94; % Input Theta 3 (Yellow)
 q3 = deg2rad(q3d_global) - offset;
 
-% --- 2. K Constants (Inverse: b is Input) ---
+% ==========================================
+% SECTION 2: LOOP 1 (Green-Yellow-Grey)
+% Input: Yellow (Theta 3)
+% ==========================================
+a = L2_Loop1; b = L3_Loop1; c = L4_Shared;
+
+% K Constants (Input b)
 K1 = d/b;
 K2 = d/c;
 K3 = (b^2 - a^2 + c^2 + d^2)/(2*b*c);
 K4 = d/a;
 K5 = (c^2 - d^2 - b^2 - a^2)/(2*b*a);
 
-% --- 3. Coefficients for Theta 4 (Grey) ---
+% Coefficients for Theta 4 (Grey)
 A = cos(q3) - K1 - K2*cos(q3) + K3;
 B = -2*sin(q3);
 C = K1 - (K2+1)*cos(q3) + K3;
 
-% --- 4. Coefficients for Theta 2 (Green) ---
+% Coefficients for Theta 2 (Green)
 D = cos(q3) - K1 + K4*cos(q3) + K5;
 E = -2*sin(q3);
 F = K1 + (K4-1)*cos(q3) + K5;
 
-% --- 5. Solve Angles (Loop 1) ---
-% Case 1 (Open): Minus Sqrt for q4, Minus Sqrt for q2
+% Solve Angles (Case 1: Open / Case 2: Crossed)
+% Note: Using Minus/Minus for Open based on typical assembly
 q4_L1_open = 2*atan((-B - sqrt(B^2 - 4*A*C))/(2*A));
 q2_L1_open = 2*atan((-E - sqrt(E^2 - 4*D*F))/(2*D));
 
-% Case 2 (Crossed): Plus Sqrt for q4, Plus Sqrt for q2
 q4_L1_cross = 2*atan((-B + sqrt(B^2 - 4*A*C))/(2*A));
 q2_L1_cross = 2*atan((-E + sqrt(E^2 - 4*D*F))/(2*D));
 
-% --- 6. Vectors Loop 1 ---
+% ==========================================
+% SECTION 3: LOOP 2 (Cyan-Red-Grey)
+% Input: Grey (Theta 4 from Loop 1)
+% Logic: Red must be BELOW
+% ==========================================
+a = L2_Loop2; b = L3_Loop2; c = L4_Shared;
+
+% K Constants (Input c - Grey)
+K1_L2 = d/c;
+K2_L2 = d/a;
+K3_L2 = (c^2 - b^2 + a^2 + d^2)/(2*c*a);
+K4_L2 = d/b;
+K5_L2 = (a^2 - d^2 - c^2 - b^2)/(2*c*b);
+
+% --- Case 1 Calculation ---
+q_in_1 = q4_L1_open; 
+% Coeffs Theta 2 (Cyan)
+A_L2_1 = cos(q_in_1) - K1_L2 - K2_L2*cos(q_in_1) + K3_L2;
+B_L2_1 = -2*sin(q_in_1);
+C_L2_1 = K1_L2 - (K2_L2+1)*cos(q_in_1) + K3_L2;
+% Coeffs Theta 3 (Red)
+D_L2_1 = cos(q_in_1) - K1_L2 + K4_L2*cos(q_in_1) + K5_L2;
+E_L2_1 = -2*sin(q_in_1);
+F_L2_1 = K1_L2 + (K4_L2-1)*cos(q_in_1) + K5_L2;
+
+% Solve: Choose roots to place Red link "Below" and Cyan "Down"
+q2_Cyan_1 = 2*atan((-B_L2_1 - sqrt(B_L2_1^2 - 4*A_L2_1*C_L2_1))/(2*A_L2_1)); 
+q3_Red_1  = 2*atan((-E_L2_1 - sqrt(E_L2_1^2 - 4*D_L2_1*F_L2_1))/(2*D_L2_1));
+
+% --- Case 2 Calculation ---
+q_in_2 = q4_L1_cross;
+% Coeffs Theta 2 (Cyan)
+A_L2_2 = cos(q_in_2) - K1_L2 - K2_L2*cos(q_in_2) + K3_L2;
+B_L2_2 = -2*sin(q_in_2);
+C_L2_2 = K1_L2 - (K2_L2+1)*cos(q_in_2) + K3_L2;
+% Coeffs Theta 3 (Red)
+D_L2_2 = cos(q_in_2) - K1_L2 + K4_L2*cos(q_in_2) + K5_L2;
+E_L2_2 = -2*sin(q_in_2);
+F_L2_2 = K1_L2 + (K4_L2-1)*cos(q_in_2) + K5_L2;
+
+% Solve
+q2_Cyan_2 = 2*atan((-B_L2_2 + sqrt(B_L2_2^2 - 4*A_L2_2*C_L2_2))/(2*A_L2_2)); 
+q3_Red_2  = 2*atan((-E_L2_2 + sqrt(E_L2_2^2 - 4*D_L2_2*F_L2_2))/(2*D_L2_2));
+
+% ==========================================
+% SECTION 4: LOOP 3 (Cyan-Blue-Brown)
+% Input: Cyan from Loop 2 (+180 deg for Straight Line)
+% ==========================================
+a = L2_Loop3; b = L3_Loop3; c = L4_Loop3;
+
+% K Constants (Input a - Cyan)
+K1_L3 = d/a;
+K2_L3 = d/c;
+K3_L3 = (a^2 - b^2 + c^2 + d^2)/(2*a*c);
+K4_L3 = d/b;
+K5_L3 = (c^2 - d^2 - a^2 - b^2)/(2*a*b);
+
+% --- Case 1 Calculation ---
+% FORCE STRAIGHT LINE: Add pi (180 deg) to Cyan angle from Loop 2
+q_in_L3_1 = q2_Cyan_1 + pi; 
+
+% Coeffs Theta 4 (Brown)
+A_L3_1 = cos(q_in_L3_1) - K1_L3 - K2_L3*cos(q_in_L3_1) + K3_L3;
+B_L3_1 = -2*sin(q_in_L3_1);
+C_L3_1 = K1_L3 - (K2_L3+1)*cos(q_in_L3_1) + K3_L3;
+% Coeffs Theta 3 (Blue)
+D_L3_1 = cos(q_in_L3_1) - K1_L3 + K4_L3*cos(q_in_L3_1) + K5_L3;
+E_L3_1 = -2*sin(q_in_L3_1);
+F_L3_1 = K1_L3 + (K4_L3-1)*cos(q_in_L3_1) + K5_L3;
+
+% Solve (Standard Open configuration)
+q4_Brown_1 = 2*atan((-B_L3_1 - sqrt(B_L3_1^2 - 4*A_L3_1*C_L3_1))/(2*A_L3_1));
+q3_Blue_1  = 2*atan((-E_L3_1 - sqrt(E_L3_1^2 - 4*D_L3_1*F_L3_1))/(2*D_L3_1));
+
+% --- Case 2 Calculation ---
+q_in_L3_2 = q2_Cyan_2 + pi; % Force Straight Line
+
+% Coeffs Theta 4 (Brown)
+A_L3_2 = cos(q_in_L3_2) - K1_L3 - K2_L3*cos(q_in_L3_2) + K3_L3;
+B_L3_2 = -2*sin(q_in_L3_2);
+C_L3_2 = K1_L3 - (K2_L3+1)*cos(q_in_L3_2) + K3_L3;
+% Coeffs Theta 3 (Blue)
+D_L3_2 = cos(q_in_L3_2) - K1_L3 + K4_L3*cos(q_in_L3_2) + K5_L3;
+E_L3_2 = -2*sin(q_in_L3_2);
+F_L3_2 = K1_L3 + (K4_L3-1)*cos(q_in_L3_2) + K5_L3;
+
+% Solve
+q4_Brown_2 = 2*atan((-B_L3_2 + sqrt(B_L3_2^2 - 4*A_L3_2*C_L3_2))/(2*A_L3_2));
+q3_Blue_2  = 2*atan((-E_L3_2 + sqrt(E_L3_2^2 - 4*D_L3_2*F_L3_2))/(2*D_L3_2));
+
+% ==========================================
+% SECTION 5: VECTORS & PLOTTING
+% ==========================================
 RO4O2 = d*exp(j*offset);
-
-% Open
-RA_L1_open = a*exp(j*(q2_L1_open + offset));
-RBA_L1_open = b*exp(j*(q3 + offset));
-RBO4_L1_open = c*exp(j*(q4_L1_open + offset));
-
-% Crossed
-RA_L1_cross = a*exp(j*(q2_L1_cross + offset));
-RBA_L1_cross = b*exp(j*(q3 + offset));
-RBO4_L1_cross = c*exp(j*(q4_L1_cross + offset));
-
-
-% ==========================================
-% LOOP 2: Cyan-Red-Grey
-% Input: Theta 4 (Grey from Loop 1)
-% Output: Theta 2 (Cyan Down), Theta 3 (Red)
-% ==========================================
-
-% --- 1. Parameters ---
-L2_new = 0.118; % a (Cyan)
-L3_new = 0.210; % b (Red)
-L4_new = 0.118; % c (Grey - Input)
-a = L2_new; b = L3_new; c = L4_new; d = L1;
-
-% --- 2. K Constants (Inverse: Link 4/c is Input) ---
-K1 = d/c; 
-K2 = d/a;
-K3 = (c^2 - b^2 + a^2 + d^2)/(2*c*a); 
-K4 = d/b;
-K5 = (a^2 - d^2 - c^2 - b^2)/(2*c*b);
-
-% --- 3. Case 1 Open (Input from L1 Open) ---
-q_in_open = q4_L1_open;
-
-% Coeffs for Theta 2 (Cyan)
-A_L2_open = cos(q_in_open) - K1 - K2*cos(q_in_open) + K3;
-B_L2_open = -2*sin(q_in_open);
-C_L2_open = K1 - (K2+1)*cos(q_in_open) + K3;
-
-% Coeffs for Theta 3 (Red)
-D_L2_open = cos(q_in_open) - K1 + K4*cos(q_in_open) + K5;
-E_L2_open = -2*sin(q_in_open);
-F_L2_open = K1 + (K4-1)*cos(q_in_open) + K5;
-
-% Solve (Use Minus Sqrt to force Cyan DOWN)
-q2_L2_open = 2*atan((-B_L2_open - sqrt(B_L2_open^2 - 4*A_L2_open*C_L2_open))/(2*A_L2_open));
-q3_L2_open = 2*atan((-E_L2_open - sqrt(E_L2_open^2 - 4*D_L2_open*F_L2_open))/(2*D_L2_open));
-
-% --- 4. Case 2 Crossed (Input from L1 Crossed) ---
-q_in_cross = q4_L1_cross;
-
-% Coeffs for Theta 2 (Cyan)
-A_L2_cross = cos(q_in_cross) - K1 - K2*cos(q_in_cross) + K3;
-B_L2_cross = -2*sin(q_in_cross);
-C_L2_cross = K1 - (K2+1)*cos(q_in_cross) + K3;
-
-% Coeffs for Theta 3 (Red)
-D_L2_cross = cos(q_in_cross) - K1 + K4*cos(q_in_cross) + K5;
-E_L2_cross = -2*sin(q_in_cross);
-F_L2_cross = K1 + (K4-1)*cos(q_in_cross) + K5;
-
-% Solve (Use Minus Sqrt to force Cyan DOWN)
-q2_L2_cross = 2*atan((-B_L2_cross - sqrt(B_L2_cross^2 - 4*A_L2_cross*C_L2_cross))/(2*A_L2_cross));
-q3_L2_cross = 2*atan((-E_L2_cross - sqrt(E_L2_cross^2 - 4*D_L2_cross*F_L2_cross))/(2*D_L2_cross));
-
-% --- 5. Vectors Loop 2 ---
-% Open
-RA_L2_open = a*exp(j*(q2_L2_open + offset));
-RBA_L2_open = b*exp(j*(q3_L2_open + offset));
-RBO4_L2_open = c*exp(j*(q_in_open + offset));
-
-% Crossed
-RA_L2_cross = a*exp(j*(q2_L2_cross + offset));
-RBA_L2_cross = b*exp(j*(q3_L2_cross + offset));
-RBO4_L2_cross = c*exp(j*(q_in_cross + offset));
-
-
-% ==========================================
-% LOOP 3: Cyan-Blue-Brown
-% Input: Theta 2 (Cyan from Loop 2 but UP)
-% Output: Theta 3 (Blue), Theta 4 (Brown)
-% ==========================================
-
-% --- 1. Parameters ---
-L2_L3 = 0.118; % a (Cyan Input)
-L3_L3 = 0.210; % b (Blue)
-L4_L3 = 0.118; % c (Brown)
-a = L2_L3; b = L3_L3; c = L4_L3; d = L1;
-
-% --- 2. K Constants (Forward: Link 2/a is Input) ---
-K1 = d/a;
-K2 = d/c;
-K3 = (a^2 - b^2 + c^2 + d^2)/(2*a*c);
-K4 = d/b;
-K5 = (c^2 - d^2 - a^2 - b^2)/(2*a*b);
-
-% --- 3. Case 1 Open (Input from L2 Open * -1) ---
-q_in_L3_open = -q2_L2_open; % Flip to point UP
-
-% Coeffs for Theta 4 (Brown)
-A_L3_open = cos(q_in_L3_open) - K1 - K2*cos(q_in_L3_open) + K3;
-B_L3_open = -2*sin(q_in_L3_open);
-C_L3_open = K1 - (K2+1)*cos(q_in_L3_open) + K3;
-
-% Coeffs for Theta 3 (Blue)
-D_L3_open = cos(q_in_L3_open) - K1 + K4*cos(q_in_L3_open) + K5;
-E_L3_open = -2*sin(q_in_L3_open);
-F_L3_open = K1 + (K4-1)*cos(q_in_L3_open) + K5;
-
-% Solve (Minus Sqrt for Open)
-q4_L3_open = 2*atan((-B_L3_open - sqrt(B_L3_open^2 - 4*A_L3_open*C_L3_open))/(2*A_L3_open));
-q3_L3_open = 2*atan((-E_L3_open - sqrt(E_L3_open^2 - 4*D_L3_open*F_L3_open))/(2*D_L3_open));
-
-% --- 4. Case 2 Crossed (Input from L2 Crossed * -1) ---
-q_in_L3_cross = -q2_L2_cross; % Flip to point UP
-
-% Coeffs for Theta 4 (Brown)
-A_L3_cross = cos(q_in_L3_cross) - K1 - K2*cos(q_in_L3_cross) + K3;
-B_L3_cross = -2*sin(q_in_L3_cross);
-C_L3_cross = K1 - (K2+1)*cos(q_in_L3_cross) + K3;
-
-% Coeffs for Theta 3 (Blue)
-D_L3_cross = cos(q_in_L3_cross) - K1 + K4*cos(q_in_L3_cross) + K5;
-E_L3_cross = -2*sin(q_in_L3_cross);
-F_L3_cross = K1 + (K4-1)*cos(q_in_L3_cross) + K5;
-
-% Solve (Plus Sqrt for Crossed)
-q4_L3_cross = 2*atan((-B_L3_cross + sqrt(B_L3_cross^2 - 4*A_L3_cross*C_L3_cross))/(2*A_L3_cross));
-q3_L3_cross = 2*atan((-E_L3_cross + sqrt(E_L3_cross^2 - 4*D_L3_cross*F_L3_cross))/(2*D_L3_cross));
-
-% --- 5. Vectors Loop 3 ---
-% Open
-RA_L3_open = a*exp(j*(q_in_L3_open + offset));
-RBA_L3_open = b*exp(j*(q3_L3_open + offset));
-RBO4_L3_open = c*exp(j*(q4_L3_open + offset));
-
-% Crossed
-RA_L3_cross = a*exp(j*(q_in_L3_cross + offset));
-RBA_L3_cross = b*exp(j*(q3_L3_cross + offset));
-RBO4_L3_cross = c*exp(j*(q4_L3_cross + offset));
-
-
-% ==========================================
-% PLOTTING
-% ==========================================
 RO4O2x = real(RO4O2); RO4O2y = imag(RO4O2);
 
-% --- Figure 1: Case 1 (Open Circuit) ---
+% --- Calculate Vectors Case 1 ---
+V_Green_1 = L2_Loop1 * exp(j*(q2_L1_open + offset));
+V_Yellow_1 = L3_Loop1 * exp(j*(q3 + offset));
+V_Grey_1 = L4_Shared * exp(j*(q4_L1_open + offset));
+
+V_Cyan_Down_1 = L2_Loop2 * exp(j*(q2_Cyan_1 + offset));
+V_Red_1 = L3_Loop2 * exp(j*(q3_Red_1 + offset));
+
+V_Cyan_Up_1 = L2_Loop3 * exp(j*(q_in_L3_1 + offset));
+V_Blue_1 = L3_Loop3 * exp(j*(q3_Blue_1 + offset));
+V_Brown_1 = L4_Loop3 * exp(j*(q4_Brown_1 + offset));
+
+% --- Calculate Vectors Case 2 ---
+V_Green_2 = L2_Loop1 * exp(j*(q2_L1_cross + offset));
+V_Yellow_2 = L3_Loop1 * exp(j*(q3 + offset));
+V_Grey_2 = L4_Shared * exp(j*(q4_L1_cross + offset));
+
+V_Cyan_Down_2 = L2_Loop2 * exp(j*(q2_Cyan_2 + offset));
+V_Red_2 = L3_Loop2 * exp(j*(q3_Red_2 + offset));
+
+V_Cyan_Up_2 = L2_Loop3 * exp(j*(q_in_L3_2 + offset));
+V_Blue_2 = L3_Loop3 * exp(j*(q3_Blue_2 + offset));
+V_Brown_2 = L4_Loop3 * exp(j*(q4_Brown_2 + offset));
+
+% --- Plot Case 1 ---
 figure(1)
-title('Complete Mechanism: Case 1 (Open)'); hold on;
-% Ground
-quiver(0,0, RO4O2x, RO4O2y, 0, 'Color', [1 0 1], 'MaxHeadSize', 0.5, 'LineWidth', 4); % Pink
-
+title('Case 1: Open Circuit (Straight Cyan Line)'); hold on;
+quiver(0,0, RO4O2x, RO4O2y, 0, 'Color', [1 0 1], 'LineWidth', 4, 'MaxHeadSize', 0.5);  % Pink
 % Loop 1
-quiver(0,0, real(RA_L1_open), imag(RA_L1_open), 0, 'green', 'MaxHeadSize', 0.5, 'LineWidth', 3); % Green
-quiver(real(RA_L1_open), imag(RA_L1_open), real(RBA_L1_open), imag(RBA_L1_open), 0, 'yellow', 'MaxHeadSize', 0.5, 'LineWidth', 3); % Yellow
-quiver(RO4O2x, RO4O2y, real(RBO4_L1_open), imag(RBO4_L1_open), 0, 'Color', [0.5 0.5 0.5], 'MaxHeadSize', 0.5, 'LineWidth', 3); % Grey
-
-% Loop 2 (Attached to Grey)
-quiver(0,0, real(RA_L2_open), imag(RA_L2_open), 0, 'cyan', 'MaxHeadSize', 0.5, 'LineWidth', 3); % Cyan (Down)
-quiver(real(RA_L2_open), imag(RA_L2_open), real(RBA_L2_open), imag(RBA_L2_open), 0, 'red', 'MaxHeadSize', 0.5, 'LineWidth', 3); % Red
-
-% Loop 3 (Attached to Cyan Up - Logic Mirror)
-quiver(0,0, real(RA_L3_open), imag(RA_L3_open), 0, 'cyan', 'MaxHeadSize', 0.5, 'LineWidth', 3, 'LineStyle', ':'); % Cyan (Up)
-quiver(real(RA_L3_open), imag(RA_L3_open), real(RBA_L3_open), imag(RBA_L3_open), 0, 'blue', 'MaxHeadSize', 0.5, 'LineWidth', 3); % Blue
-quiver(RO4O2x, RO4O2y, real(RBO4_L3_open), imag(RBO4_L3_open), 0, 'Color', [0.85 0.5 0.1], 'MaxHeadSize', 0.5, 'LineWidth', 3); % Brown
-
+quiver(0,0, real(V_Green_1), imag(V_Green_1), 0, 'green', 'LineWidth', 3, 'MaxHeadSize', 0.5);
+quiver(real(V_Green_1), imag(V_Green_1), real(V_Yellow_1), imag(V_Yellow_1), 0, 'yellow', 'LineWidth', 3, 'MaxHeadSize', 0.5);
+quiver(RO4O2x, RO4O2y, real(V_Grey_1), imag(V_Grey_1), 0, 'Color', [0.5 0.5 0.5], 'LineWidth', 3, 'MaxHeadSize', 0.5);
+% Loop 2 (Cyan Down, Red Below)
+quiver(0,0, real(V_Cyan_Down_1), imag(V_Cyan_Down_1), 0, 'cyan', 'LineWidth', 3, 'MaxHeadSize', 0.5);
+quiver(real(V_Cyan_Down_1), imag(V_Cyan_Down_1), real(V_Red_1), imag(V_Red_1), 0, 'red', 'LineWidth', 3, 'MaxHeadSize', 0.5);
+% Loop 3 (Cyan Up, Straight from Loop 2)
+quiver(0,0, real(V_Cyan_Up_1), imag(V_Cyan_Up_1), 0, 'cyan', 'LineWidth', 3, 'MaxHeadSize', 0.5, 'LineStyle', ':');
+quiver(real(V_Cyan_Up_1), imag(V_Cyan_Up_1), real(V_Blue_1), imag(V_Blue_1), 0, 'blue', 'LineWidth', 3, 'MaxHeadSize', 0.5);
+quiver(RO4O2x, RO4O2y, real(V_Brown_1), imag(V_Brown_1), 0, 'Color', [0.85 0.5 0.1], 'LineWidth', 3, 'MaxHeadSize', 0.5);
 axis equal; grid on;
 
-% --- Figure 2: Case 2 (Crossed Circuit) ---
+% --- Plot Case 2 ---
 figure(2)
-title('Complete Mechanism: Case 2 (Crossed)'); hold on;
-% Ground
-quiver(0,0, RO4O2x, RO4O2y, 0, 'Color', [1 0 1], 'MaxHeadSize', 0.5, 'LineWidth', 4); % Pink
-
+title('Case 2: Crossed Circuit (Straight Cyan Line)'); hold on;
+quiver(0,0, RO4O2x, RO4O2y, 0, 'Color', [1 0 1], 'LineWidth', 4, 'MaxHeadSize', 0.5);  % Pink
 % Loop 1
-quiver(0,0, real(RA_L1_cross), imag(RA_L1_cross), 0, 'green', 'MaxHeadSize', 0.5, 'LineWidth', 3); 
-quiver(real(RA_L1_cross), imag(RA_L1_cross), real(RBA_L1_cross), imag(RBA_L1_cross), 0, 'yellow', 'MaxHeadSize', 0.5, 'LineWidth', 3); 
-quiver(RO4O2x, RO4O2y, real(RBO4_L1_cross), imag(RBO4_L1_cross), 0, 'Color', [0.5 0.5 0.5], 'MaxHeadSize', 0.5, 'LineWidth', 3); 
-
+quiver(0,0, real(V_Green_2), imag(V_Green_2), 0, 'green', 'LineWidth', 3, 'MaxHeadSize', 0.5);
+quiver(real(V_Green_2), imag(V_Green_2), real(V_Yellow_2), imag(V_Yellow_2), 0, 'yellow', 'LineWidth', 3, 'MaxHeadSize', 0.5);
+quiver(RO4O2x, RO4O2y, real(V_Grey_2), imag(V_Grey_2), 0, 'Color', [0.5 0.5 0.5], 'LineWidth', 3, 'MaxHeadSize', 0.5);
 % Loop 2
-quiver(0,0, real(RA_L2_cross), imag(RA_L2_cross), 0, 'cyan', 'MaxHeadSize', 0.5, 'LineWidth', 3); 
-quiver(real(RA_L2_cross), imag(RA_L2_cross), real(RBA_L2_cross), imag(RBA_L2_cross), 0, 'red', 'MaxHeadSize', 0.5, 'LineWidth', 3); 
-
+quiver(0,0, real(V_Cyan_Down_2), imag(V_Cyan_Down_2), 0, 'cyan', 'LineWidth', 3, 'MaxHeadSize', 0.5);
+quiver(real(V_Cyan_Down_2), imag(V_Cyan_Down_2), real(V_Red_2), imag(V_Red_2), 0, 'red', 'LineWidth', 3, 'MaxHeadSize', 0.5);
 % Loop 3
-quiver(0,0, real(RA_L3_cross), imag(RA_L3_cross), 0, 'cyan', 'MaxHeadSize', 0.5, 'LineWidth', 3, 'LineStyle', ':'); 
-quiver(real(RA_L3_cross), imag(RA_L3_cross), real(RBA_L3_cross), imag(RBA_L3_cross), 0, 'blue', 'MaxHeadSize', 0.5, 'LineWidth', 3); 
-quiver(RO4O2x, RO4O2y, real(RBO4_L3_cross), imag(RBO4_L3_cross), 0, 'Color', [0.85 0.5 0.1], 'MaxHeadSize', 0.5, 'LineWidth', 3); 
-
+quiver(0,0, real(V_Cyan_Up_2), imag(V_Cyan_Up_2), 0, 'cyan', 'LineWidth', 3, 'MaxHeadSize', 0.5, 'LineStyle', ':');
+quiver(real(V_Cyan_Up_2), imag(V_Cyan_Up_2), real(V_Blue_2), imag(V_Blue_2), 0, 'blue', 'LineWidth', 3, 'MaxHeadSize', 0.5);
+quiver(RO4O2x, RO4O2y, real(V_Brown_2), imag(V_Brown_2), 0, 'Color', [0.85 0.5 0.1], 'LineWidth', 3, 'MaxHeadSize', 0.5);
 axis equal; grid on;
 
-% --- DISPLAY RESULTS ---
-disp('--- FINAL ANGLE RESULTS (Degrees) ---');
-disp(' ');
-disp('CASE 1 (OPEN):');
-disp(['  Green (L1 Crank): ', num2str(rad2deg(q2_L1_open) + offset_deg)]);
-disp(['  Grey  (L1 Rocker): ', num2str(rad2deg(q4_L1_open) + offset_deg)]);
-disp(['  Cyan  (L2 Crank): ', num2str(rad2deg(q2_L2_open) + offset_deg)]);
-disp(['  Red   (L2 Coupler): ', num2str(rad2deg(q3_L2_open) + offset_deg)]);
-disp(['  Blue  (L3 Coupler): ', num2str(rad2deg(q3_L3_open) + offset_deg)]);
-disp(['  Brown (L3 Rocker): ', num2str(rad2deg(q4_L3_open) + offset_deg)]);
-disp(' ');
-disp('CASE 2 (CROSSED):');
-disp(['  Green (L1 Crank): ', num2str(rad2deg(q2_L1_cross) + offset_deg)]);
-disp(['  Grey  (L1 Rocker): ', num2str(rad2deg(q4_L1_cross) + offset_deg)]);
-disp(['  Cyan  (L2 Crank): ', num2str(rad2deg(q2_L2_cross) + offset_deg)]);
-disp(['  Red   (L2 Coupler): ', num2str(rad2deg(q3_L2_cross) + offset_deg)]);
-disp(['  Blue  (L3 Coupler): ', num2str(rad2deg(q3_L3_cross) + offset_deg)]);
-disp(['  Brown (L3 Rocker): ', num2str(rad2deg(q4_L3_cross) + offset_deg)]);
+% Results
+disp('--- Results ---');
+disp(['Case 1 Green: ', num2str(rad2deg(q2_L1_open)+offset_deg)]);
+disp(['Case 1 Cyan(Up): ', num2str(rad2deg(q_in_L3_1)+offset_deg)]);
